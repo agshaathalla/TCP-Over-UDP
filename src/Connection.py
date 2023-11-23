883416e85f45d49fac8de984277ec923573c690e
Eunice Sarah Siregar <13521013@mahasiswa.itb.ac.id>
2023-11-23 18:20:57 +0700

    feat: connection
    Co-authored-by: Agsha Athalla Nurkareem <agshaathalla@users.noreply.github.com>

---
+++ src/Connection.py
@@ -0,0 +1,29 @@
+import socket
+from MessageInfo import MessageInfo
+from Segment import Segment
+class Connection:
+    def __init__(self, ip: str, port: int):
+        self.ip = ip
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind((self.ip, self.port))
+    
+    def send(self, ip_remote: str, port_remote: int, msg: Segment):
+            self.socket.sendto(msg.get_bytes(), (ip_remote, port_remote))
+            print("Data sent successfully!")
+
+    def listen(self):
+            self.socket.settimeout(999)
+            data, addr = self.socket.recvfrom(32756)
+            print(data.decode("utf-8"))
+            return data, addr 
+    
+    def close(self):
+        self.socket.close()
+    
+    def register_handler(self, handler: callable(MessageInfo)):
+        self.handler = handler
+    
+    def notify(self):
+        print(f"ip: {self.ip}, port: {self.port}, handler: {self.handler}")
+
---
+++ src/MessageInfo.py
@@ -0,0 +1,7 @@
+from Segment import Segment
+
+class MessageInfo:
+    def __init__(self, ip: str, port: int, segment: Segment):
+        self.ip = ip
+        self.port = port
+        self.segment = segment
